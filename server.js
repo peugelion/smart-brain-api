1f38a89e1a07530fb9f7df8906956c6d7ef5054f
peugelion80 <jovica.jovanovic@europos.co.rs>
2020-01-15 11:57:20 +0100

    Changed db connection config

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const cors = require('cors');
 const db = require('knex')({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
-    user : 'postgres',
-    password : 'admin',
-    database : 'smart-brain'
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
